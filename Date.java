2f6a537b6c54dfaaca9f0dcb0472d8dc815e82b4
davide cascapera <101217500+elperon90@users.noreply.github.com>
2022-04-04 14:42:39 +0200

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,30 @@
+package esercizi_thinkjava;
+// esercizio 2.2
+public class Date {
+	public static void main(String[] args) {
+
+		String day = "Monday";
+		int date = 05;
+		String month = "April";
+		int year = 2022;
+		System.out.print("The American format is ");
+		System.out.print(day);
+		System.out.print(", ");
+		System.out.print(month);
+		System.out.print(date);
+		System.out.print(", ");
+		System.out.println(year);
+
+		System.out.print("The European format is ");
+		System.out.print(day );
+		System.out.print(date );
+		System.out.print(month);
+		System.out.println(year);
+
+
+	}
+}	
+
+
+
+
